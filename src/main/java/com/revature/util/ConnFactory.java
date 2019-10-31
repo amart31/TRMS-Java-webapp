d339ef90ede118c4308a17b02d8ca06a407e2a93
Andy Martinez <martinez1751@gmail.com>
2019-10-31 14:07:57 -0400

    ConnFactory done

--- src/main/java/com/revature/util/ConnFactory.java
+++ src/main/java/com/revature/util/ConnFactory.java
@@ -1,5 +1,43 @@
 package com.revature.util;
 
-public class ConnFactory {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class ConnFactory {
+	private static ConnFactory cf = new ConnFactory();
+	
+	private ConnFactory() {
+		super();
+	}
+	
+	public static synchronized ConnFactory getInstance() {
+		if(cf==null) {
+			cf = new ConnFactory();		
+		}
+		return cf;	
+	}
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		Properties prop= new Properties();
+		
+		try {
+			prop.load(new FileReader("database.properties"));
+			conn= DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("password"));
+			
+		} catch (SQLException e) {
+			System.out.println("failed to create connection");
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
